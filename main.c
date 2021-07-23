4be167868c3016c1b5a7aa4184b27311e884a21a
mlebrun <mlebrun@student.42.fr>
2021-07-23 15:51:56 +0200

    feat: try function

--- main.c
+++ main.c
@@ -2,22 +2,38 @@
 #include <time.h>
 #include <sys/time.h>
 #include <pthread.h>
+#include <unistd.h>
 
-void	*test()
+int		fork_nb = 3;
+
+void	*test(void *data)
 {
-		printf("yo la zone\n");
-		return ("YES");
+	return ("YES");
 }
 
 int	main(int argc, char **argv)
 {
-	pthread_t t1, t2;
+	pthread_t th[3];
+	int			i;
 
 	(void)argv;
+	i = 0;
 	if (argc < 5 || argc > 6)
 		printf("Error arguments: NUMBER_OF_PHILOSOPHERS TIME_TO_DIE TIME_TO_EAT TIME_TO_SLEEP [NUMBER_OF_TIMES_EACH_PHILOSOPHER_MUST_EAT]\n");
-	pthread_create(&thread_id, NULL, &test, "coucou");
-	pthread_join(thread_id, NULL);
+	while  (i < 3)
+	{
+		if (i == 0)
+			pthread_create(&th[i], NULL, &test, "oui");
+		else if (i == 1)
+			pthread_create(&th[i], NULL, &test, "non");
+		else if (i == 2)
+			pthread_create(&th[i], NULL, &test, "ptet");
+		i++;
+	}
+
+	pthread_join(th[0], NULL);
+	pthread_join(th[1], NULL);
+	pthread_join(th[2], NULL);
 	printf("this is the end\n");
 	return (0);
 }
